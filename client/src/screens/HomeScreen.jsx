3f249ab65f26327b5ab476ab2baa43f383515f63
VedangKanade <vedangk20@gmail.com>
2024-01-18 17:24:22 +0530

    reswitched form vite app to react js

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,24 +1,24 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Grid, Heading } from "@chakra-ui/react";
 
 const HomeScreen = () => {
   return (
-    <Flex direction="row">
-      <Flex
+    <Flex direction="row" w="100vw" h="100vh" bgColor="#000000">
+      <Flex /* SideBAR */
         direction="column"
-        w="350px"
-        h="720"
+        w="300px"
+        h="100vh"
         alignItems="start"
         bgColor="green.400"
       >
-        <Heading as="h1" size="2xl">
-          Spotify Clone
-        </Heading>
+        <Flex direction="column">
+          <Grid height="300px" templateColumns="repeat(2, 1fr)">
+            <Flex bgColor="yellow"></Flex>
+            <Flex bgColor="red"></Flex>
+          </Grid>
+        </Flex>
       </Flex>
-      <Flex h="720" w="1200px" bgColor="black">
-        <Heading color="green.400" as="h1" size="2xl">
-          {" "}
-          Gana sun na re :{" "}
-        </Heading>
+      <Flex w="500px" h="100vh">
+        <Heading>Hello</Heading>
       </Flex>
     </Flex>
   );
