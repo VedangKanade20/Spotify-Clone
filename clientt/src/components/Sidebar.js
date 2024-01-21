a2c6f795cb70632759fd5c96522fdc3264f5f17b
VedangKanade <vedangk20@gmail.com>
2024-01-21 22:59:49 +0530

    Added Logo

--- clientt/src/components/Sidebar.js
+++ clientt/src/components/Sidebar.js
@@ -1,14 +1,15 @@
-import { Divider, Flex, Heading } from "@chakra-ui/react";
+import { Divider, Image, Flex, Heading } from "@chakra-ui/react";
 import { FiSearch } from "react-icons/fi";
 import { MdHome } from "react-icons/md";
-import { image } from "../images/mainLOGO.png";
+import IMAGE from "../images/mainLOGO.png";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <Flex
       direction="column"
       alignItems="center"
-      w="20vw"
+      w="15vw"
       bgColor="gray.600"
       borderRadius="15"
     >
@@ -23,7 +24,13 @@ const Sidebar = () => {
         bgColor="black"
         borderRadius="10"
       >
-        
+        <Image
+          src={IMAGE}
+          objectFit="cover"
+          width="150px"
+          height="32px"
+          borderRadius="10"
+        />
       </Flex>
       <Flex
         dir="column"
@@ -38,6 +45,7 @@ const Sidebar = () => {
       >
         <MdHome size="20" color="white" />
         <Heading as="h3" size="md" color="white">
+          <Link />
           Home
         </Heading>
       </Flex>
