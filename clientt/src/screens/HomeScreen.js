76fa06b0d085093959617ff2d8fd09a3b87ac62f
VedangKanade <vedangk20@gmail.com>
2024-01-27 01:32:10 +0530

    3

--- clientt/src/screens/HomeScreen.js
+++ clientt/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { Divider, Flex, Heading, Text } from "@chakra-ui/react";
+import { Divider, Flex, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
 import Sidebar from "../components/Sidebar";
 import { IoChevronForwardCircleOutline } from "react-icons/io5";
 import { IoChevronBackCircleOutline } from "react-icons/io5";
@@ -10,38 +10,49 @@ const HomeScreen = () => {
 
       <Flex
         direction="column"
-        w="100%"
+        w="90%"
         h="90vh"
         bgColor="#121212"
         borderRadius="12px"
-        mr="2"
+        mr="1"
       >
         {/* Header */}
         <Flex
           direction="row"
           bgColor="#121212"
-          w="100% "
+          w="80wh"
           h="7vh"
           borderRadius="12px"
         >
-          <Flex w="73vw" gap="2">
+          <Flex w="100%" gap="2">
             <IoChevronBackCircleOutline color="white" size="50" />
             <IoChevronForwardCircleOutline color="white" size="50" />
           </Flex>
-          <Flex direction="row " justifyItems="end" alignItems="center" gap="3">
+          <Flex
+            direction="row "
+            justifyItems="space-between"
+            alignItems="center"
+            gap="8"
+          >
             <Flex
-              direction="row"
               gap="3"
               borderRadius="10px"
               alignItems="center"
-              w="60px"
+              w="66px"
+              h="40px"
             >
               <Text fontWeight="bold" as="h3" size="xl" color="white">
                 Sign Up
               </Text>
             </Flex>
             <Divider orientation="vertical" size="2" />
-            <Flex direction="row" gap="5" borderRadius="10px" w="40px">
+            <Flex
+              gap="3  "
+              borderRadius="10px"
+              w="66px"
+              alignItems="center"
+              h="40px"
+            >
               <Text fontWeight="bold" as="h3" size="xl" color="white">
                 Login
               </Text>
@@ -51,22 +62,33 @@ const HomeScreen = () => {
         {/* MAIN */}
         <Flex direction="column" bgColor="#1f1f1f" w="100%" h="100%">
           <Flex
-            w="83vw"
+            w="100%"
             h="5vh"
             alignItems="center"
             justifyContent="space-between"
+            mt="10px"
           >
             <Flex w="fit-content">
-              <Text textColor="white" as="h3" fontWeight="bold" size="xl">
+              <Text textColor="gray" as="h3" fontWeight="bold" fontSize="2xl">
                 Spotify Playlist
               </Text>
             </Flex>
             <Flex w="fit-content" justifyContent="end">
-              <Text textColor="white" as="h3" fontWeight="bold" size="xl">
+              <Text textColor="gray" as="h3" fontWeight="bold" fontSize="2xl">
                 Show All
               </Text>
             </Flex>
           </Flex>
+
+          <Grid templateRows="1fr 1fr 1fr 1fr" gap="6">
+            <GridItem w="100px" h="50px" bgColor="blue.50" />
+            <GridItem w="100px" h="50px" bgColor="blue.50" />
+            <GridItem w="100px" h="50px" bgColor="blue.50" />
+            <GridItem w="100px" h="50px" bgColor="blue.50" />
+            <GridItem w="100px" h="50px" bgColor="blue.50" />
+            <GridItem w="100px" h="50px" bgColor="blue.50" />
+            <GridItem w="100px" h="50px" bgColor="blue.50" />
+          </Grid>
         </Flex>
       </Flex>
     </Flex>
